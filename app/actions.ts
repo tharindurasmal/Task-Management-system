7df197d495a9d68eed21539f32b96218f890b797
tharindurasmal <tharindurasmal1@gmail.com>
2026-09-15 12:40:37 +0530

    acction check  user is login or not

--- app/actions.ts
+++ app/actions.ts
@@ -2,15 +2,28 @@
 
 import prisma from "@/lib/prisma";
 import { revalidatePath } from "next/cache";
+import { getServerSession } from "next-auth";
 
 export async function createTask(formData: FormData) {
+  const session = await getServerSession();
+  if(!session?.user?.email) {
+    throw new Error("You must be logged in to create a task.");
+  }
+
   const title = formData.get("title") as string;
   
   if (!title || title.trim() === "") return;
 
+  const user = await prisma.user.findUnique({
+    where: { email: session.user.email },
+  });
+  if (!user) {
+    throw new Error("User not found.");
+  }
+
   // create new task in db
   await prisma.task.create({
-    data: { title },
+    data: { title, userId: user.id },
   });
 
   // refresh page
